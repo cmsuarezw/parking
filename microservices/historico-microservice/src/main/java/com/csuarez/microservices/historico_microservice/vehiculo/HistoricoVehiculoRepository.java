817f1d4a3b70a94cc3f265d85934ee85f5840654
cmsuarezw <gnerico.zw70@gmail.com>
2026-06-21 16:58:33 +0200

    Eliminado el historico para rehacerlo de nuevo

--- microservices/historico-microservice/src/main/java/com/csuarez/microservices/historico_microservice/vehiculo/HistoricoVehiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csuarez.microservices.historico_microservice.vehiculo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistoricoVehiculoRepository extends JpaRepository<HistoricoVehiculoEntity, Long>{
-
-}
